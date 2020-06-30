c857c548851edbef7b5b76878b6a40f79d887da8
Arthur Pain <arthur.pain@epitech.eu>
2020-06-30 18:21:15 +0200

    🎨 aze

--- src/app/@Services/indexDb.service.ts
+++ src/app/@Services/indexDb.service.ts
@@ -22,7 +22,7 @@ export class IndexDbService {
   }
 
   private init() {
-    const request = window.indexedDB.open('cvd', 203);
+    const request = window.indexedDB.open('op', 203);
     request.onsuccess = (event: any) => {
       this.db = event.target.result;
       this.dbReady$.next(true);
