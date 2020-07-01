8d08bc452f462defe484a8d1618c9d07a065f606
Arthur Pain <arthur.pain@epitech.eu>
2020-07-01 11:48:20 +0200

    🎨 Add modals for notification

--- src/app/@Services/local-notification.service.ts
+++ src/app/@Services/local-notification.service.ts
@@ -1,27 +1,42 @@
 import {Injectable} from '@angular/core';
 import {NotificationElement} from '../@Models/notification.model';
+import {AlertDialogComponent} from '../@Components/dialogs/alert-dialog/alert-dialog.component';
+import {OverlayService} from './overlay.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LocalNotificationService {
 
   private hasPermission = false;
 
-  constructor() {
-    Notification.requestPermission((status) => {
+  constructor(private overlayService: OverlayService) {
+    if (!Notification) {
+      this.showError('Notifications', 'Push notifications are not available in your content');
+    }
+    Notification.requestPermission().then((status) => {
       console.log('Notification permission status:', status);
       this.hasPermission = true;
+    }, (err) => {
+      this.showError('Notifications', 'Push notification rejected');
     });
   }
 
   send(title: string, notification: NotificationElement) {
-    if (Notification.permission === 'granted') {
+    if (this.hasPermission && Notification.permission === 'granted') {
       navigator.serviceWorker.getRegistration().then((reg) => {
         if (reg) {
           reg.showNotification(title, notification);
         }
       });
     }
+  }
+
+  showError(title: string, message) {
+    this.overlayService.open(AlertDialogComponent, {
+      title,
+      message,
+      emoji: '⚠️',
+    });
   }
 }
